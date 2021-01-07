5416be60c6c741ac252a3c719871fb69f90525a9
RuthieMartin714 <allisonruthmartin714@gmail.com>
2021-01-06 23:13:12 -0600

    open menu on icon click

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ function handleMenuClick(){
     let menu = document.querySelector(".menu");
     menu.classList.add("open");
 }
+
+let menuIcon = document.querySelector(".mobile-menu-icon-white");
+menuIcon.addEventListener("click", handleMenuClick);
