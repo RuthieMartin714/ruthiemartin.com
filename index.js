6da825568a753a549fd25766e0b99fba8964c351
RuthieMartin714 <allisonruthmartin714@gmail.com>
2021-01-13 20:29:47 -0600

    made menu close

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-function handleMenuClick(){
+function openMenu(){
     let menu = document.querySelector(".menu");
     menu.classList.add("open");
 }
 
+function closeMenu(){
+    let menu = document.querySelector(".menu");
+    menu.classList.remove("open");
+}
+
 let menuIcon = document.querySelector(".mobile-menu-icon-white");
-menuIcon.addEventListener("click", handleMenuClick);
+menuIcon.addEventListener("click", openMenu);
+
+let menuIconClose = document.querySelector(".menu-footer-icon");
+menuIconClose.addEventListener("click", closeMenu);
